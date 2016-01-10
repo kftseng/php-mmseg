fbe81f9451df1c7cb8bf67a655d66bf0b5a2c99e
root <root@dev2.amigo.com.tw>
2016-01-10 11:59:12 +0800

    merge from zhangjingpu/php-mmseg branch delete seg so to release momery

--- mmseg/mmseg.cpp
+++ mmseg/mmseg.cpp
@@ -325,6 +325,10 @@ PHP_FUNCTION(mmseg_segment)
         add_next_index_stringl(return_value, tok, len);
         seg->popToken(len);
     }
+
+    // delete seg so to release momery
+    mgr->clear();
+
     return ;
 }
 /* }}} */
